c615e89c14afe0c9c84c4def1d2949161b6653c3
DeveloperOwala <otienoerick46@gmail.com>
2021-09-18 16:08:27 +0300

    creating class for animals sighted

---
+++ src/main/java/appModels/animalSighted.java
@@ -0,0 +1,15 @@
+package appModels;
+
+public class animalSighted {
+    private String location;
+    private String rangerName;
+    private int wildlifeId;
+    private int id;
+
+    public animalSighted(String location, String rangerName, int wildlifeId ) {
+        this.location = location;
+        this.rangerName = rangerName;
+        this.wildlifeId = wildlifeId;
+
+    }
+}
