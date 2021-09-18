af0adc466f6b6f0683bc9a2191b49531920ff017
DeveloperOwala <otienoerick46@gmail.com>
2021-09-18 14:34:12 +0300

    creating attributes for animal class

--- src/main/java/appModels/Animal.java
+++ src/main/java/appModels/Animal.java
@@ -3,5 +3,8 @@ package appModels;
 public class Animal {
     private static final String young = "";
     private  static final String type="";
+    private int id;
+    public static String DATABASE_TYPE ="Animal";
+
 
 }
